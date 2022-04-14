0ed6e0d746795b5850c244bbe476dcda17242e49
lucas-mior <lucas.mior@tutamail.com>
2022-04-13 22:38:42 -0300

    patch: alpha

--- config.h
+++ config.h
@@ -105,6 +105,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -215,6 +218,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_F11,         changealpha,    {.f = -0.05} },
+	{ ControlMask,          XK_F12,         changealpha,    {.f = +0.05} },
 };
 
 /*
